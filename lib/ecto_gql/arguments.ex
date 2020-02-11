842c2a57add6d1c7b0def7bca9336c7f6f593f69
Oskar Köök <oskarkook@maatriks.ee>
2020-02-11 17:25:55 +0200

    filters

--- lib/ecto_gql/arguments.ex
+++ lib/ecto_gql/arguments.ex
@@ -3,15 +3,11 @@ defmodule EctoGQL.Arguments do
   require EctoGQL.Filters
 
   def create_argument_handler(argument_name, filter, field) do
-    if !Kernel.function_exported?(EctoGQL.Filters, filter, 3) do
-      raise "No filter called #{inspect(filter)}"
-    end
-
-    filter_fn = quote do: Function.capture(EctoGQL.Filters, unquote(filter), 3)
+    filter_fn = {:function, filter, [], []}
 
     quote do
       def handle_argument(unquote(argument_name), query, value) do
-        unquote(filter_fn).(query, unquote(field), value)
+        unquote(filter_fn)(query, unquote(field), value)
       end
     end
   end
